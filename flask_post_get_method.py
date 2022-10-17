f3eddd268f8bc3e93162dc9dd013b11abc54c1c9
engr-junaid-qazi <engr.junaid.qazi@hotmail.com>
2022-10-17 16:00:23 +0500

    first_deployment

---
+++ flask_post_get_method.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.htm') 
+
+@app.route("/contact")
+def contact(): 
+    return render_template('contact.html')  
+
+@app.route("/info",methods=['GET','POST'])
+def info():
+    if request.method == "GET":
+        return "<h1>GET Method</h1>Hello "+ request.args.get('user') + \
+    " Your Email:"+request.args.get('email')
+    else:
+        return "<h1>POST Method</h1>Hello "+ request.form['user'] + \
+    " Your Email:"+request.form['email']
+    
+
+
+app.run(debug=True) 
